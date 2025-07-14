69b0a4c05182f9de8b69a797b5fe4d2a35458145
Mehtap Hisarciklilar <68424810+hisarciklilar@users.noreply.github.com>
2025-07-14 23:47:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 from app_pages.page_summary import page_summary_body
+from app_pages.page_house_price_study import page_house_price_study_body
 
 app = MultiPage(app_name="House Prices")
 
 app.add_page("Quick Project Summary", page_summary_body)
-
+app.add_page("Explore House Prices", page_house_price_study_body)
 app.run()
