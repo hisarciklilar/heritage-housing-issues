0ef4dd0e2b4bd525493d8df18e1ef6c95e0c890c
Mehtap Hisarciklilar <68424810+hisarciklilar@users.noreply.github.com>
2025-07-14 23:45:02 +0000

    Add house_price_study.py

---
+++ app_pages/page_house_price_study.py
@@ -0,0 +1,20 @@
+import numpy as np
+import streamlit as st
+from src.data_management import load_house_price_data
+
+def page_house_price_study_body():
+
+    df = load_house_price_data()
+
+    st.write("*** House Price Exploratory Analysis")
+    st.info(
+        f"* The client is interested in discovering how the house attributes correlate with sale price\n"
+    )
+
+    if st.checkbox("Inspect house prices and attributes"):
+        st.write(
+            f"* The dataset has {df.shape[0]} observations and {df.shape[1]} variables, "
+            f"find below the first 10 rows."
+        )
+
+        st.write(df.head(10))
