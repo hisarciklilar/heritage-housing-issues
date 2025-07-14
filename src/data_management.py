e7f016391265dda4bfc710ee4f84105c22657921
Mehtap Hisarciklilar <68424810+hisarciklilar@users.noreply.github.com>
2025-07-14 23:35:57 +0000

    Add src folder and data_management.py

---
+++ src/data_management.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+@st.cache_data
+def load_house_price_data():
+    df=pd.read_csv('outputs/datasets/collection/house_prices_records.csv')
+    return df
