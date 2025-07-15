293aea7e000a6e92ec4ee2ef335e8bdaf4bb7b26
Mehtap Hisarciklilar <68424810+hisarciklilar@users.noreply.github.com>
2025-07-15 09:37:57 +0000

    Add correlation heatmap to st app

--- app_pages/page_correlations.py
+++ app_pages/page_correlations.py
@@ -1,7 +1,7 @@
 import numpy as np
 import streamlit as st
-#import seaborn as sns
-#import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
 from src.data_management import load_house_price_data
 
 def page_correlations():
@@ -23,6 +23,21 @@ def page_correlations():
     if st.checkbox("Inspect correlation of house price with house features"):
         numeric_cols = df.select_dtypes(include=np.number).columns
         corr = df[numeric_cols].corr()['SalePrice'].sort_values(key=abs, ascending=False)[1:]
+
         st.write(corr)
 
     st.write("---")
+
+    if st.checkbox("Reveal correlation heatmap of house price and features"):
+
+        numeric_cols = df.select_dtypes(include=np.number).columns
+        corr = df[numeric_cols].corr()
+
+        mask = np.triu(np.ones_like(corr, dtype=bool))
+
+        fig, ax = plt.subplots(figsize=(12,10))
+        sns.heatmap(corr, cmap="coolwarm", center=0, annot=True, fmt="0.2f", 
+            mask=mask, linewidths=1, cbar_kws={"shrink": 0.75}
+        )
+        plt.title("Correlation Heatmap")
+        st.pyplot(fig)
